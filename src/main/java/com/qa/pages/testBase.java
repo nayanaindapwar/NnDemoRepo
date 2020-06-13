ea9a476bf20de83ceb060633d72360071416bba2
nayana indapwar <nayanaindapwar>
2020-06-13 23:15:48 +0530

    My first commit for NnDemoGitRepo

---
+++ src/main/java/com/qa/pages/testBase.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class testBase {
+	
+	public void test() {
+		System.out.println("This is test base class");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/test1.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("This is test1 class");
+	}
+
+}
